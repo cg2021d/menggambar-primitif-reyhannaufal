33289ceef6d076d621de36f7aff9a7eb3bbdaedf
Reyhan Naufal <reyhannaufal21@gmail.com>
2021-09-14 20:09:46 +0700

    feat: draw triangle

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ function main() {
   const canvas = document.getElementById("canvas");
   const gl = canvas.getContext("webgl");
 
+  /*
+   * Definisikan vertex
+   * WebGL hanya sampai 0-1
+   * (0.0, 0.5), B (0.5, -0.5), C (-0.5, -0.5)
+   */
+
+  const vertices = [0.0, 0.5, 0.5, -0.5, -0.5, -0.5];
+
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
   const vertexShaderCode = `
+        attribute vec2 a_Position;
         void main() {
-            gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-            gl_PointSize = 10.0;
+        gl_Position = vec4(a_Position, 0.0, 1.0);
+            gl_PointSize = 20.0;
         }
     `;
 
@@ -15,7 +29,7 @@ function main() {
 
   const fragmentShaderCode = `
         void main() {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
         }
     `;
 
@@ -29,8 +43,13 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
+  gl.clearColor(1.0, 1.0, 1.0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
